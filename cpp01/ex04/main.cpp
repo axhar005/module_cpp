b1a769bd4165e1608962828689b23a1989643cd2
oboucher <oboucher@c3r5p6.42quebec.com>
2023-12-15 16:30:43 -0500

    update

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,40 +1,47 @@
 #include <fstream>
 #include <iostream>
 #include "inc/colors.hpp"
-std::string av_to_string(char *a){
-	std::string str;
 
-	for (int i = 0; a[i]; i++)
-		str += std::toupper(a[i]);
-	return (str);
+std::string readFile(std::string filename, std::string &text){
+	std::ifstream infile(filename);
+	std::string line;
+	if (infile.is_open()){
+		while (getline(infile, line))
+			text += line + "\n";
+		text = text.substr(0, text.length() - 1);
+	}else
+		std::cout << RED << "No permission or file not found" << RESET << std::endl;
+	infile.close();
+	return (text);
+}
+
+void writeFile(std::string filename, std::string &text){
+	std::ofstream outfile(filename.append(".replace"));
+	if (outfile.is_open())
+		outfile << text;
+	else
+		std::cerr << RED << "file not found" << std::endl;
+	outfile.close();
+}
+
+void changeText(std::string &text, std::string old_word, std::string replace_word){
+	size_t pos = text.find(old_word);
+	while (pos != std::string::npos) {
+		text.erase(pos, old_word.length());
+		text.insert(pos, replace_word);
+		pos = text.find(old_word, pos + replace_word.length());
+	}
 }
 
 int main(int ac, char **av){
-	
-	if (ac <= 1)
+	if (ac != 4)
 		return (0);
 	std::string text;
-	std::string line;
-	std::ifstream infile(av_to_string(av[1]));
-	
-	// read file
-	if (infile.is_open()){
-		while (getline(infile, line)) {
-			text += line + "\n";
-		}
-		text = text.substr(0, text.length() - 1);
-	}else{
-		std::cout << RED << "No permission or file not found" << RESET << std::endl;
-	}
-
-	// change text
-	int start = 0;
-	while (text.find(av[2], start)) {
-	
-	}
-
-
-	// write new file
-
+	std::string filename(av[1]);
+	std::string old_word(av[2]);
+	std::string replace_word(av[3]);
+	readFile(filename, text);
+	changeText(text, old_word, replace_word);
+	writeFile(filename, text);
 	return (0);
 }
